2c10e8dd46cfc776617e700f1c1b33cc6067bf28
Ankit Kumar <ankitsharmauitbu@gmail.com>
2024-07-02 23:17:55 +0530

    Added spring security

---
+++ src/main/java/com/ankit/webservice/user/SpringSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.ankit.webservice.user;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		// All requests should be authenticated
+		http.authorizeHttpRequests(auth->auth.anyRequest().authenticated());
+		
+		// define type of authentication ( basic auth )
+		http.httpBasic();
+		
+		// disable csrf because for post and put request usually we have to give csrf tokens
+		http.csrf().disable();
+		
+		return http.build();
+	}
+}
